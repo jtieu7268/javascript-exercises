73f6468071d1a3c14c2ebacce7258d5c5e85d884
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-26 15:26:58 -0400

    Complete 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(lower, upper) {
+    if (typeof(lower) !== "number" || 
+        typeof(upper) !== "number" ||
+        lower < 0 ||
+        upper < 0 ||
+        !Number.isInteger(lower) ||
+        !Number.isInteger(upper)) 
+        return 'ERROR';
+    if (upper < lower) [lower, upper] = [upper, lower];
+    let sum = 0;
+    for (let i = lower; i <= upper; i++) {
+        sum += i;
+    }
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
