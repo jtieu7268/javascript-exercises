83505668f5f7659c79c3bd9cfcceb89411eeb3e5
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-26 15:40:46 -0400

    Complete 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,24 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    /* brute force logic
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    } else {
+        return false;
+    }
+    */
+
+    // readable version
+    const isYearDivisibleByFour = year % 4 === 0;
+    const isYearDivisibleByOneHundred = year % 100 === 0;
+    const isYearDivisibleByFourHundred = year % 400 === 0;
+
+    return isYearDivisibleByFour && !isYearDivisibleByOneHundred || isYearDivisibleByFourHundred;
 
 };
 
