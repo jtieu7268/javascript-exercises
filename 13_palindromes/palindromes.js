aa9d0e51c4946a917434db32f700b6f825b5b428
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-29 13:26:25 -0400

    Complete 12_palinfromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,6 @@
-const palindromes = function () {
-
-};
-
+const palindromes = function (str) {
+    cleanstr = str.toLowerCase().replaceAll(/[^a-z0-9]/g, "");
+    return cleanstr === cleanstr.split('').reverse().join('');
+}
 // Do not edit below this line
 module.exports = palindromes;
