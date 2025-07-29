2d500cf2e2053847726784c6aaf633625aaa0b4f
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-29 13:43:56 -0400

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num == 0) return 0;
+    let fibnminus1 = 0;
+    let fibn = 1;
+    for (let i = 1; i < num; i++) {
+        let curr = fibnminus1 + fibn;
+        fibnminus1 = fibn;
+        fibn = curr;
+    }
+    return fibn;
 };
 
 // Do not edit below this line
