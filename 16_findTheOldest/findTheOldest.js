7899082e9a9fa0e1bafca8d5a006c0ebc86502a1
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-07-29 14:16:56 -0400

    16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((old, cur) => {
+        let oldAge = getAge(old.yearOfBirth, old.yearOfDeath);
+        let curAge = getAge(cur.yearOfBirth, cur.yearOfDeath);
+        return oldAge > curAge ? old : cur; 
+    });
 };
+
+function getAge(birth, death) {
+    death = death === undefined ? (new Date()).getFullYear() : death;
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
